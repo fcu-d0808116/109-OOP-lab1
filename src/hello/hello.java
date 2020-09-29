70604600584d862a3357463f5d1d3c01010d11bd
fcu-d0808116 <72053243+fcu-d0808116@users.noreply.github.com>
2020-09-29 09:41:21 +0800

    Update hello.java

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm ¹ù­õÃv");
+		System.out.println("I'm solomonliao");
 		System.out.println("My nid is D0808116");
 		System.out.println("This is lab 1 hw");
 	}
