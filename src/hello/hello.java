4156d2d99ce912080c0908309cf0b88108bfbbfb
user <user@EA03>
2020-09-29 09:38:17 +0800

    123

---
+++ src/hello/hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm ¹ù­õÃv");
+		System.out.println("My nid is D0808116");
+		System.out.println("This is lab 1 hw");
+	}
+
+}
